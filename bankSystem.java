cdfcd27ae38fb95e03f5d0e32d57f7133c98a6f1
Laith Hijazi <laith.hijazi@icloud.com>
2025-01-07 18:24:16 +0300

    add fields to bankSystem class

---
+++ bankSystem.java
@@ -0,0 +1,7 @@
+import java.util.HashMap;
+
+public class bankSystem{
+    private HashMap<String, User> users = new HashMap<>();
+    private HashMap<String, Account> accounts = new HashMap<>();
+    private HashMap<String, Account> userAccounts = new HashMap<>();
+}
