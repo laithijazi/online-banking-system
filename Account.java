e949bc2e2a2c5e48421dea451cabbc10b41dc632
Laith Hijazi <laith.hijazi@icloud.com>
2025-01-07 18:12:25 +0300

    add constructor to Account class

--- Account.java
+++ Account.java
@@ -2,4 +2,10 @@ public class Account{
     private String accountNumber;
     private String userId;
     private double balance;
+
+    public Account(String accountNumber, String userId, double initialBalance){
+        this.accountNumber = accountNumber;
+        this.userId = userId;
+        this.balance = initialBalance;
+    }
 }
