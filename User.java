cbcd1dbd89cf466bd85cd52fc9581665eb55e6e7
Laith Hijazi <laith.hijazi@icloud.com>
2025-01-07 17:59:17 +0300

    add constructor to User class

--- User.java
+++ User.java
@@ -3,4 +3,11 @@ public class User{
     private String name;
     private String email;
     private String password;
+
+    public User(String userId, String name, String email, String password){
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
 }
