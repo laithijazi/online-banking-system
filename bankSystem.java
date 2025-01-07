0a176eb7dd87092902d23bab22c4046179376af3
Laith Hijazi <laith.hijazi@icloud.com>
2025-01-07 18:30:32 +0300

    add getters to bankSystem class

--- bankSystem.java
+++ bankSystem.java
@@ -4,4 +4,14 @@ public class bankSystem{
     private HashMap<String, User> users = new HashMap<>();
     private HashMap<String, Account> accounts = new HashMap<>();
     private HashMap<String, Account> userAccounts = new HashMap<>();
+
+    public HashMap<String, User> getUsers() {
+        return users;
+    }
+    public HashMap<String, Account> getAccounts() {
+        return accounts;
+    }
+    public HashMap<String, Account> getUserAccounts() {
+        return userAccounts;
+    }
 }
