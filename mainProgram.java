cf9f0ab3dd92f90b7bd6e4f7919cfb8db57ea68a
Laith Hijazi <laith.hijazi@icloud.com>
2025-01-07 19:39:19 +0300

    test user and account creation in mainProgram

--- mainProgram.java
+++ mainProgram.java
@@ -1,1 +1,10 @@
+public class mainProgram{
+    public static void main(String args[]){
+        bankSystem bank = new bankSystem();
+        bank.createUser("64220039", "Laith Hijazi", "laith.hijazi@icloud.com", "123123");
+        bank.createAccount("1", "64220039", 500);
 
+        bank.createUser("64220068", "Hassan Ghezzaoui", "hassan.ghezzaoui@gmail.com", "53215321");
+        bank.createAccount("2", "64220068", 300);
+    }
+}
