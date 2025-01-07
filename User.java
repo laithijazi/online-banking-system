2c2db5d94c728bd097359972bdaca65be0580d58
Laith Hijazi <laith.hijazi@icloud.com>
2025-01-07 18:03:50 +0300

    add getters and setters to User class

--- User.java
+++ User.java
@@ -10,4 +10,36 @@ public class User{
         this.email = email;
         this.password = password;
     }
+
+    public String getUserId(){
+        return userId;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setUserId(String userId){
+        this.userId = userId;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }    
 }
