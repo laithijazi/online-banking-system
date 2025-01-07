a72437677613bb35f9bb75814ac6285acc41e77e
Laith Hijazi <laith.hijazi@icloud.com>
2025-01-07 18:15:06 +0300

    add getters and setters to Account class

--- Account.java
+++ Account.java
@@ -8,4 +8,28 @@ public class Account{
         this.userId = userId;
         this.balance = initialBalance;
     }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
