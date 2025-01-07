6f945c9ea58a2ff58088259c374ca080332bd110
Laith Hijazi <laith.hijazi@icloud.com>
2025-01-07 18:46:58 +0300

    create createUser method to bankSystem class

--- bankSystem.java
+++ bankSystem.java
@@ -14,4 +14,15 @@ public class bankSystem{
     public HashMap<String, Account> getUserAccounts() {
         return userAccounts;
     }
+
+    public void createUser(String userId, String name, String email, String password){
+        if(users.containsKey(userId)){
+            System.out.println("User already exists.");
+        }
+        else{
+            User user = new User(userId, name, email, password);
+            users.put(userId, user);
+            System.out.println("User has been created for " + user.getName());
+        }
+    }
 }
