f5bedaf4e3a35df38e15acc1c60f4e1263980eda
Laith Hijazi <laith.hijazi@icloud.com>
2025-01-08 09:43:56 +0300

    add fields to Transaction class

--- Transaction.java
+++ Transaction.java
@@ -1,3 +1,8 @@
+import java.time.LocalDateTime;
+
 public class Transaction{
-    
+    private String type;
+    private double amount;
+    private LocalDateTime date;
+    private Account account;
 }
