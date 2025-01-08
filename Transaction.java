aeffcf9b5aec8f792d2b2a90bb907ad7ed01c790
Laith Hijazi <laith.hijazi@icloud.com>
2025-01-08 09:52:07 +0300

    add getters and setter to Transaction class

--- Transaction.java
+++ Transaction.java
@@ -12,4 +12,37 @@ public class Transaction{
         this.date = LocalDateTime.now();
         this.account = account;
     }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+    
 }
